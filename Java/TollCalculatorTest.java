125434a8142feb4bce2e831cce77d46e7a10b449
Oskar Gotte <oskar.gotte@gmail.com>
2018-05-17 17:59:08 +0200

    Add tests for multi passes by vehicle

--- Java/TollCalculatorTest.java
+++ Java/TollCalculatorTest.java
@@ -1,4 +1,8 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 import java.time.*;
 import java.util.Date;
 
@@ -7,6 +11,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class TollCalculatorTest {
 
     TollCalculator tollCalculator = new TollCalculator(new TollFeeTimeIntervalPolicyEvolve());
+    TollCalculator tollCalculatorHighFeeAllDay;
+
     private static final int HIGH_TOLL_FEE = TollFeeTimeIntervalPolicyEvolve.HIGH_TOLL_FEE_EVOLVE;
     private static final int MEDIUM_TOLL_FEE = TollFeeTimeIntervalPolicyEvolve.MEDIUM_TOLL_FEE_EVOLVE;
     private static final int LOW_TOLL_FEE = TollFeeTimeIntervalPolicyEvolve.LOW_TOLL_FEE_EVOLVE;
@@ -15,6 +21,14 @@ class TollCalculatorTest {
 
     private static final LocalDate WEEKDAY_NON_HOLIDAY = LocalDate.of(2018, 5, 2);
     private static final Vehicle NORMAL_FEE_VEHICLE = new Car();
+
+    @BeforeEach
+    void beforeEach() {
+        TollFeeTimeIntervalPolicy highFeeAllDayPolicy = Mockito.mock(TollFeeTimeIntervalPolicyBase.class);
+        Mockito.when(highFeeAllDayPolicy.getTollFee(Mockito.any())).thenReturn(HIGH_TOLL_FEE);
+
+        tollCalculatorHighFeeAllDay = new TollCalculator(highFeeAllDayPolicy);
+    }
 
     @Test
     void ShouldBeHighFeeOnRushHours() {
@@ -46,6 +60,55 @@ class TollCalculatorTest {
     void ShouldBeNoFeeOnEveningAndNight() {
         AssertSameFeeForTimeInterval(NO_FEE, LocalTime.parse("18:30"), LocalTime.parse("00:00"));
         AssertSameFeeForTimeInterval(NO_FEE, LocalTime.parse("00:00"), LocalTime.parse("06:00"));
+    }
+
+
+    @Test
+    void AVehicleShouldBeChargedForEachEntryIfMoreThanAnHourApart() {
+        Date[] entryTimesMoreThanAnHourApart = {
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("15:30")),
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("16:31")),
+        };
+
+        int fee = tollCalculatorHighFeeAllDay.getTollFee(NORMAL_FEE_VEHICLE, entryTimesMoreThanAnHourApart);
+
+        assertEquals(HIGH_TOLL_FEE * 2, fee);
+    }
+
+    @Test
+    void AVehicleShouldOnlyBeChargeOnceAnHour() {
+        Date[] entryTimesWithinAnHour = {
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("06:45")),
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("07:45")),
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("15:30")),
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("16:30")),
+        };
+        int fee = tollCalculatorHighFeeAllDay.getTollFee(NORMAL_FEE_VEHICLE, entryTimesWithinAnHour);
+
+        assertEquals(HIGH_TOLL_FEE * 2, fee);
+    }
+
+    @Test
+    void AVehicleShouldOnlyBeChargeOnceAnHourWhenPassingAtDifferentDays() {
+        Date[] entryTimesWithinAnHour = {
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("23:45")),
+                toDate(WEEKDAY_NON_HOLIDAY.plusDays(1), LocalTime.parse("00:15")),
+        };
+        int fee = tollCalculatorHighFeeAllDay.getTollFee(NORMAL_FEE_VEHICLE, entryTimesWithinAnHour);
+
+        assertEquals(HIGH_TOLL_FEE, fee);
+    }
+
+    @Test
+    void ShouldBeHighestFeeWhenMultiplePassesPerHour() {
+        /* TODO: Is this really a requirement?? */
+        Date[] entryTimesWithDifferentFeesWithinAnHour = {
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("15:15")), // 13 kr
+                toDate(WEEKDAY_NON_HOLIDAY, LocalTime.parse("15:45")), // 18 kr
+        };
+        int fee = tollCalculator.getTollFee(NORMAL_FEE_VEHICLE, entryTimesWithDifferentFeesWithinAnHour);
+
+        assertEquals(HIGH_TOLL_FEE, fee);
     }
 
     void AssertSameFeeForTimeInterval(int expectedFee, LocalTime intervalStart, LocalTime intervalEnd) {
